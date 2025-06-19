42d1bc0df2ea019b6005167c86068b325b350123
p-arkaprava <imapaul05@gmail.com>
2025-06-19 08:51:23 +0530

    Initial commit

---
+++ Sequence_detector.srcs/sim_1/new/tb_Sequence_Detector_Moore_FSM_Verilog.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.06.2025 08:33:18
+// Design Name: 
+// Module Name: tb_Sequence_Detector_Moore_FSM_Verilog
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module tb_Sequence_Detector_Moore_FSM_Verilog;
+
+ // Inputs
+ reg sequence_in;
+ reg clock;
+ reg reset;
+
+ // Outputs
+ wire detector_out;
+
+ // Instantiate the Sequence Detector using Moore FSM
+ Sequence_Detector_MOORE_Verilog uut (
+  .sequence_in(sequence_in), 
+  .clock(clock), 
+  .reset(reset), 
+  .detector_out(detector_out)
+ );
+ initial begin
+ clock = 0;
+ forever #5 clock = ~clock;
+ end 
+ initial begin
+  // Initialize Inputs
+  sequence_in = 0;
+  reset = 1;
+  // Wait 100 ns for global reset to finish
+  #30;
+      reset = 0;
+  #40;
+  sequence_in = 1;
+  #10;
+  sequence_in = 0;
+  #10;
+  sequence_in = 1; 
+  #20;
+  sequence_in = 0; 
+  #20;
+  sequence_in = 1; 
+  #20;
+  sequence_in = 0;  
+  // Add stimulus here
+
+ end
+      
+endmodule
